bf04a1c4bbc1971e03c43fd3784fc3b21d1a19aa
Faridha <mullafaridhabanu@gmail.com>
2025-01-05 11:59:02 +0530

    Add files via upload

---
+++ pattern8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int b=5;
+    while(b>=1){
+        int a=b;
+        while(a>=1){
+            printf("%d",a);
+            a=a-1;
+        }
+        printf("\n");
+        b=b-1;
+    }
+}
